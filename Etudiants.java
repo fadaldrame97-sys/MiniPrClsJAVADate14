75e974b3ac40d0616b9553d30de27b2121b8a746
fadaldrame97-sys <fadaldrame97@gmail.com>
2025-11-18 22:34:43 +0100

    Add files via upload

---
+++ Etudiants.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Etudiants {
+    private int Id;
+    public String nom;
+    ArrayList<Cours> ListeCoursSuivis=new ArrayList<>();
+
+    public Etudiants(int    Id,String nom){
+        this.Id=Id;
+        this.nom=nom;
+        this.ListeCoursSuivis=ListeCoursSuivis;
+    }
+    public Etudiants ( ){
+         Id= 68;
+         nom= "Babacar Faye";}
+  public void EtudiantsSuiventCours(){
+      if(ArrayList<Cours>   )
+
+  }
+}
+class  Cours{
+    private int Id;
+    public String titre;
+    ArrayList<Etudiants> EtudiansInscrits=new ArrayList<>();
+
+    public Cours (int Id, String titre){
+        this.Id=Id;
+        this.titre=titre;
+    }
+    public Cours(){
+        Id= 25;
+        titre="Informatque";
+    }
+
+public void ajouterEtudiant(){
+
+
+    }
+public void InscrireDesEtudiant(){
+
+
+}
+}
+
+public void main() {
+
+    Etudiants etudiants=new Etudiants();
+    Cours cours=new Cours();
+
+
+
+}
